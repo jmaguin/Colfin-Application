6a1c7e206a70dd5f5bec9fa1f988e03038dfc766
Percieval <eric.yanjun.huang@gmail.com>
2023-05-15 16:53:24 -0700

    Eric Update

--- Flutter_finance_app_ui-main/lib/pages/transection_page.dart
+++ Flutter_finance_app_ui-main/lib/pages/transection_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_finance_app/pages/constants.dart';
 import 'package:flutter_finance_app/theme/colors.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
@@ -13,6 +14,21 @@ class TransectionPage extends StatefulWidget {
 }
 
 class _TransectionPageState extends State<TransectionPage> {
+  late Future<ApiConstant> futureAlbum;
+  bool avail = false;
+  @override
+  void initState() {
+    super.initState();
+    try{
+      futureAlbum = fetchAlbum();
+      avail = true;
+    }catch(e){
+      avail = false;
+    }
+    
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,24 +79,29 @@ class _TransectionPageState extends State<TransectionPage> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Recent Transaction",
+                Text("Coupons",
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 20,
+                      fontSize: 25,
                       color: mainFontColor,
                     )),
-                Text("See all",
+                avail? Text("Service Available",
                     style: TextStyle(
                       fontWeight: FontWeight.w600,
                       fontSize: 13,
                       color: mainFontColor,
-                    )),
+                    )): Text("Service Not Available",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 13,
+                      color: mainFontColor,
+                    )), 
               ],
             ),
           ),
          
          Padding(
-           padding: const EdgeInsets.only(top:5,bottom: 20,left: 25,right: 25),
+           padding: const EdgeInsets.only(top:5,bottom: 20,left: 25,right: 20),
            child: Column(children: [
             Row(children: [
                Container(              
@@ -116,7 +137,7 @@ class _TransectionPageState extends State<TransectionPage> {
                   ),
               child: Center(
                 child: Text(
-                  "Income",
+                  "Online Shopping",
                   style: TextStyle(
                       color: Colors.black.withOpacity(0.5),
                       fontSize: 13,
@@ -140,7 +161,7 @@ class _TransectionPageState extends State<TransectionPage> {
                   ),
               child: Center(
                 child: Text(
-                  "Expense",
+                  "Food",
                   style: TextStyle(
                       color: Colors.black.withOpacity(0.5),
                       fontSize: 13,
@@ -201,7 +222,7 @@ class _TransectionPageState extends State<TransectionPage> {
                                 //   // shape: BoxShape.circle
                                 // ),
                                 child: Center(
-                                    child: Icon(Icons.payment,color: mainFontColor,)),
+                                    child: Icon(Icons.emoji_food_beverage,color: mainFontColor,)),
                               ),
                               SizedBox(
                                 width: 10,
@@ -215,7 +236,7 @@ class _TransectionPageState extends State<TransectionPage> {
                                           CrossAxisAlignment.start,
                                       children: [
                                         Text(
-                                          "Payment",
+                                          "In N Out",
                                           style: TextStyle(
                                               fontSize: 15,
                                               color: black,
@@ -225,7 +246,7 @@ class _TransectionPageState extends State<TransectionPage> {
                                           height: 5,
                                         ),
                                         Text(
-                                          "Payment from Andrea",
+                                          "Santa Clara, CA",
                                           style: TextStyle(
                                               fontSize: 12,
                                               color: black.withOpacity(0.5),
@@ -240,7 +261,7 @@ class _TransectionPageState extends State<TransectionPage> {
                                     mainAxisAlignment: MainAxisAlignment.end,
                                     children: [
                                       Text(
-                                        "\$30.00",
+                                        "-\$5.00",
                                         style: TextStyle(
                                             fontSize: 15,
                                             fontWeight: FontWeight.bold,
@@ -248,56 +269,245 @@ class _TransectionPageState extends State<TransectionPage> {
                                       )
                                     ],
                                   ),
+                                  
                                 ),
                               )
                             ],
                           ),
+            
                         ),
+                        
                       ),
                     ),
                   ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                              top: 20, bottom: 10, right: 20, left: 20),
-                  child:
-                    Column(
-                        children:[
-                           Container(
-                              padding: EdgeInsets.only(left:25,right: 25,top: 5,bottom: 5),
-                              margin: EdgeInsets.symmetric(horizontal: 15),
-                                child: Container(
-                                          width: 325,
-                                          height: 325,
-                                          decoration: BoxDecoration(
-                                              
-                                              image: DecorationImage(
-                                                  image: NetworkImage(
-                                                      "https://res.cloudinary.com/dic3o7vzw/image/upload/v1673927487/avatars/cropped_uxagcn.jpg"),
-                                                  fit: BoxFit.cover)),
-                                        ),
+                 Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(
+                          top: 10,
+                          left: 25,
+                          right: 25,
+                        ),
+                        decoration: BoxDecoration(
+                            color: white,
+                            borderRadius: BorderRadius.circular(25),
+                            boxShadow: [
+                              BoxShadow(
+                                color: grey.withOpacity(0.03),
+                                spreadRadius: 10,
+                                blurRadius: 3,
+                                // changes position of shadow
                               ),
-                        ]
+                            ]),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              top: 10, bottom: 20, right: 20, left: 20),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 50,
+                                height: 50,
+                                // decoration: BoxDecoration(
+                                //   color: arrowbgColor,
+                                //   borderRadius: BorderRadius.circular(15),
+                                //   // shape: BoxShape.circle
+                                // ),
+                                child: Center(
+                                    child: Icon(Icons.emoji_food_beverage,color: mainFontColor,)),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  width: (size.width - 90) * 0.7,
+                                  child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Boba Guys",
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              color: black,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(
+                                          "Santa Clara, CA",
+                                          style: TextStyle(
+                                              fontSize: 12,
+                                              color: black.withOpacity(0.5),
+                                              fontWeight: FontWeight.w400),
+                                        ),
+                                      ]),
+                                ),
+                              ),
+                              Expanded(
+                                child: Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        "-10%",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                            color: black),
+                                      )
+                                    ],
+                                  ),
+                                  
+                                ),
+                              )
+                            ],
+                          ),
+            
+                        ),
+                        
                       ),
+                    ),
+                  ],
+                ),
+                 Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(
+                          top: 10,
+                          left: 25,
+                          right: 25,
+                        ),
+                        decoration: BoxDecoration(
+                            color: white,
+                            borderRadius: BorderRadius.circular(25),
+                            boxShadow: [
+                              BoxShadow(
+                                color: grey.withOpacity(0.03),
+                                spreadRadius: 10,
+                                blurRadius: 3,
+                                // changes position of shadow
+                              ),
+                            ]),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              top: 10, bottom: 20, right: 20, left: 20),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 50,
+                                height: 50,
+                                // decoration: BoxDecoration(
+                                //   color: arrowbgColor,
+                                //   borderRadius: BorderRadius.circular(15),
+                                //   // shape: BoxShape.circle
+                                // ),
+                                child: Center(
+                                    child: Icon(Icons.computer,color: mainFontColor,)),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  width: (size.width - 90) * 0.7,
+                                  child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "HP Notebook Professional Laptop",
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              color: black,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(
+                                          "Amazon",
+                                          style: TextStyle(
+                                              fontSize: 12,
+                                              color: black.withOpacity(0.5),
+                                              fontWeight: FontWeight.w400),
+                                        ),
+                                      ]),
+                                ),
+                              ),
+                              Expanded(
+                                child: Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        "-\$500.00",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                            color: black),
+                                      )
+                                    ],
+                                  ),
+                                  
+                                ),
+                              )
+                            ],
+                          ),
+            
+                        ),
+                        
+                      ),
+                    ),
+                  ],
+                ),
+                // Padding(
+                //   padding: const EdgeInsets.only(
+                //               top: 20, bottom: 10, right: 20, left: 20),
+                //   child:
+                //     Column(
+                //         children:[
+                //            Container(
+                //               padding: EdgeInsets.only(left:25,right: 25,top: 5,bottom: 5),
+                //               margin: EdgeInsets.symmetric(horizontal: 15),
+                //                 child: Container(
+                //                           width: 325,
+                //                           height: 325,
+                //                           decoration: BoxDecoration(
+                                              
+                //                               image: DecorationImage(
+                //                                   image: NetworkImage(
+                //                                       "https://res.cloudinary.com/dic3o7vzw/image/upload/v1673927487/avatars/cropped_uxagcn.jpg"),
+                //                                   fit: BoxFit.cover)),
+                //                         ),
+                //               ),
+                //         ]
+                //       ),
                     
-                ),
-                 Container(
+                // ),
+            //      Container(
               
-              padding: EdgeInsets.all(16),
-              margin: EdgeInsets.all( 25),
-              decoration: BoxDecoration(
+            //   padding: EdgeInsets.all(16),
+            //   margin: EdgeInsets.all( 25),
+            //   decoration: BoxDecoration(
                 
-                  color:buttoncolor, borderRadius: BorderRadius.circular(25)),
-              child: Center(
-                child: Text(
-                  "See Details",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600),
-                ),
-              ),
-            ),
+            //       color:buttoncolor, borderRadius: BorderRadius.circular(25)),
+            //   child: Center(
+            //     child: Text(
+            //       "See Details",
+            //       style: TextStyle(
+            //           color: Colors.white,
+            //           fontSize: 16,
+            //           fontWeight: FontWeight.w600),
+            //     ),
+            //   ),
+            // ),
         ],
       ),
     ));
